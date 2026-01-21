f24e46bc1b7aff79df5b1a33efc64f9f978988a0
PC <PC@DESKTOP-41RVR4P>
2026-01-20 16:22:41 -0800

    Add project files.

---
+++ OOP_Project_V2/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace OOP_Project_V2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            // 1)  هنسجل المواد 
+
+            Console.Write("Do you want to register courses? (Y/N): ");
+            string input = Console.ReadLine().ToUpper();
+
+            if (input == "Y" || input == "YES")
+            {
+
+            }
+
+            else
+            {
+                /* لو داس على لا هيدخل 
+             AssignGrade
+                و الفانكشن دي بتستقبل List Student 
+                List Courses 
+
+*/
+                Console.Write("Do you want to display students' grades? (Y/N): ");
+                string input2 = Console.ReadLine().ToUpper();
+
+                if (input2 == "Y" || input2 == "YES")
+                {
+
+                }
+            }
+        }
+    }
+}
